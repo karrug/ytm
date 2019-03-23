a3a27b4c2c6998d4b3cb752bc05d612795c1755e
dinesh <dinesh@micropyramid.com>
2019-03-23 10:54:46 -0400

    add slow network option

--- music/views.py
+++ music/views.py
@@ -5,21 +5,23 @@ from music import utils
 
 
 def home(request):
+    network = request.GET.get('network', 'high')
     if request.POST:
         q = request.POST['q']
         results = utils.get_results(q)
-        return render(request, 'music/home.html', {'results': results, 'q': q})
+        return render(request, 'music/home.html', {'results': results, 'q': q, 'network': network})
     else:
-        return render(request, 'music/home.html')
+        return render(request, 'music/home.html', {'network': network})
 
 
 def dinesh_home(request):
+    network = request.GET.get('network', 'high')
     if request.POST:
         q = request.POST['q']
         results = utils.get_results(q)
-        return render(request, 'music/dinesh_home.html', {'results': results, 'q': q})
+        return render(request, 'music/dinesh_home.html', {'results': results, 'q': q, 'network': network})
     else:
-        return render(request, 'music/dinesh_home.html')
+        return render(request, 'music/dinesh_home.html', {'network': network})
 
 
 def get_src(request, videoid):
