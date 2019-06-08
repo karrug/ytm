db89541b205b464805b039ea78a521c4077c6ec4
root <root@karrug-com.asia-south1-c.c.decisive-apogee-243115.internal>
2019-06-08 16:34:56 +0000

    change static path

--- ytm/urls.py
+++ ytm/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include, re_path
 
 
 urlpatterns = [
-    path('', include('music.urls')),
+    path('music', include('music.urls')),
     path('admin/', admin.site.urls),
 ]
